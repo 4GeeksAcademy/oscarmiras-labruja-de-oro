e86327dd6af3ada8d293d11db9f7775abf88fbeb
Alexander T.C <82457422+FluxFeint@users.noreply.github.com>
2023-05-24 18:07:06 +0000

    Endpoint y añadido el fichero .json

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,7 +1,8 @@
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
-from flask import Flask, request, jsonify, url_for, Blueprint
+import json
+from flask import Flask, request, jsonify, url_for, Blueprint, abort
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 
@@ -16,3 +17,19 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@app.route('/api/horoscopo/<string:signo_zodiacal>', methods=['GET'])
+def get_horoscopo(signo_zodiacal):
+    try:
+        with open('predicciones.json') as f:  # Abrimos el archivo 'predicciones.json'
+            data = json.load(f)  # Cargamos el contenido del archivo en la variable 'data'
+
+        horoscopo = data.get(signo_zodiacal)  # Obtenemos el horóscopo correspondiente al signo zodiacal
+
+        if horoscopo is not None:  # Verificamos si existe un horóscopo para el signo zodiacal
+            return jsonify(horoscopo), 200  # Devuelve el horóscopo como respuesta JSON con código 200
+        else:
+            abort(404)  # Envia una respuesta con código de error 404
+
+    except Exception as e:  # Captura las excepciones que ocurra
+        return jsonify({"error": str('No se pudo enviar el horoscopo')}), 500  # Devuelve un mensaje de error como respuesta JSON con código 500
